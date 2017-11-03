397ce7382ae9867aa5ea6000d6483feb43bedfa8
Oliver Sturm <oliver@oliversturm.com>
2017-11-03 10:52:21 +0000

    initial checkin

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import {
+  SortingState,
+  LocalSorting,
+  PagingState,
+  LocalPaging,
+  ColumnOrderState
+} from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  TableView,
+  TableHeaderRow,
+  PagingPanel,
+  DragDropContext
+} from '@devexpress/dx-react-grid-material-ui';
+
+import { columns, rows } from './data';
+
+const App = () => (
+  <div>
+    <Grid columns={columns} rows={rows}>
+      <PagingState defaultPageSize={5} />
+      <SortingState
+        defaultSorting={[{ columnName: 'name', direction: 'asc' }]}
+      />
+      <ColumnOrderState defaultOrder={['name', 'artist', 'year']} />
+      <LocalSorting />
+      <LocalPaging />
+      <DragDropContext />
+      <TableView allowColumnReordering />
+      <TableHeaderRow allowSorting allowDragging />
+      <PagingPanel allowedPageSizes={[0, 5, 10, 20]} />
+    </Grid>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
